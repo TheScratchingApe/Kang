1b67304b8b1450f2a87d16f2858fb4498be67193
TheScratchingApe <128901545+TheScratchingApe@users.noreply.github.com>
2024-12-28 00:44:45 +0100

    Update spaceinvaders.js

--- js/spaceinvaders.js
+++ js/spaceinvaders.js
@@ -14,11 +14,6 @@ var KEY_SPACE = 32;
 const SCALE = 1.6;
 const PLAYER_SCALE = 3.0;
 
-if (window.innerWidth < 900) {
-    SCALE = 1.0;
-    PLAYER_SCALE = 2.0; 
-  }
-
 // -- Images du joueur et des invaders --
 
 // Joueur
